f29942f267848050c8634035cedbc9ebc80bca81
janaSunrise <warriordefenderz@gmail.com>
2020-11-17 19:03:52 +0530

    integrate few searching features

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-from click import command, echo, group
+from click import echo, group, option
 
-from parser import wiki_random
+from parser import (
+    wiki_random,
+    wiki_languages,
+    wiki_search,
+    wiki_suggest,
+)
 
 @group()
 def cli():
@@ -11,6 +16,28 @@ def cli():
 @cli.command()
 def random():
     echo(f"A random interesting topic is {wiki_random()}")
+
+@cli.command()
+def languages():
+    echo(wiki_languages())
+
+@cli.command()
+@option('--query', prompt="The query to search in wikipedia", help="The query to search in wikipedia")
+@option('--results', default=5, help="The number of results to display")
+def search(query, results):
+    res = wiki_search(query, results)
+    echo(
+        "The results for the search are:\n\n" + '\n'.join(res)
+    )
+
+@cli.command()
+@option('--query', prompt="The query to search in wikipedia", help="The query to search in wikipedia")
+def suggestion(query):
+    res = wiki_suggest(query)
+    if res:
+        echo(f"The suggestion found for {query} are:\n\n" + '\n'.join(res))
+    else:
+        echo(f"Wupsy, The suggestion for {query} could not be found.")
 
 
 if __name__ == '__main__':
--- src/parser.py
+++ src/parser.py
@@ -12,3 +12,59 @@ def wiki_random():
     request = _get_json(params)
 
     return request["query"]["random"][0]["title"]
+
+def wiki_search(query, results=5):
+    params = {
+        'list': 'search',
+        'srprop': '',
+        'srlimit': results,
+        'limit': results,
+        'srsearch': query
+    }
+
+    raw_results = _get_json(params)
+
+    if 'error' in raw_results:
+        if raw_results['error']['info'] in ('HTTP request timed out.', 'Pool queue is full'):
+            raise Exception(query)
+        else:
+            raise Exception(raw_results['error']['info'])
+
+    search_results = (result['title'] for result in raw_results['query']['search'])
+
+    return list(search_results)
+
+def wiki_languages():
+    response = _get_json(
+        {
+            'meta': 'siteinfo',
+            'siprop': 'languages'
+        }
+    )
+
+    languages = response['query']['languages']
+
+    lang_string = ""
+    for lang in languages:
+        lang_string += f"{lang['code']}: {lang['name']}\n"
+
+    return lang_string
+
+def wiki_suggest(query):
+    params = {
+        'list': 'search',
+        'srinfo': 'suggestion',
+        'srprop': '',
+    }
+    params['srsearch'] = query
+
+    raw_result = _get_json(params)
+
+    titles = []
+    if raw_result["query"]["search"]:
+        for search in raw_result["query"]["search"]:
+            titles.append(search["title"])
+
+        return titles
+
+    return None
