ab98887b8385c4e9b7957b43139d934babea03a4
janaSunrise <warriordefenderz@gmail.com>
2020-11-17 10:38:12 +0530

    use click for designing the CLIs

---
+++ finder/__main__.py
@@ -0,0 +1,11 @@
+from .scraper import _get_json
+
+# print(
+#     _get_json(
+#         {
+#             'list': 'random',
+#             'rnnamespace': 0,
+#             'rnlimit': 1,
+#         }
+#     )
+# )
