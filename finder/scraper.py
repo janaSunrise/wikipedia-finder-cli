03e71c698c3c59aa2850b1c1d17b37c8e3a653c4
janaSunrise <warriordefenderz@gmail.com>
2020-11-17 10:14:23 +0530

    first move towards scraping

---
+++ finder/scraper.py
@@ -0,0 +1,21 @@
+import requests
+
+# test variable
+query = "adolf hitler"
+lang = "en"
+
+base_url = f"https://{lang}.wikipedia.org/w/api.php"
+
+
+def _get_json(params):
+
+    params["action"] = "query"
+    params["titles"] = query.replace(" ", "_")
+    params["format"] = "json"
+    params["formatversion"] = "2"  # Cleaner json results
+    params["prop"] = "extracts"  # Include extract in returned results
+    params["exintro"] = "1"  # Only return summary paragraph(s) before main content
+    params["redirects"] = "1"  # Follow redirects
+    params["explaintext"] = "1"  # Make sure it's plaintext (not HTML)
+
+    return requests.get(base_url, params=params).json()
